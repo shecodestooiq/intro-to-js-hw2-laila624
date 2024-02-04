a0cd7164255bb2ce06582cc07d2591aab0a00d0a
laila624 <superherrro624@gmail.com>
2024-02-04 21:14:34 +0300

    hw2 done

---
+++ index.js
@@ -0,0 +1,11 @@
+var numberA = 60 ;
+var numberB = 40;
+
+console.log(numberA > numberB);
+console.log(numberA < numberB);
+console.log(numberA >= numberB);
+console.log(numberA <= numberB);
+console.log(numberA == numberB);
+console.log(numberA != numberB);
+
+
